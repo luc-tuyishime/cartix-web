87b41dc761b7dbf5f8a2a8215ccde592da52c7e0
luc <luctunechi45@gmail.com>
2016-11-22 18:24:19 +0200

    html and css

---
+++ js/style.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "50%";
+    document.body.style.backgroundColor = "lightgray";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
