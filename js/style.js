b2f5c5fb58e45975865f58585d13d963aa30b9b9
luc <luctunechi45@gmail.com>
2016-11-23 10:03:23 +0200

    new branche

--- js/style.js
+++ js/style.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "50%";
-    document.body.style.backgroundColor = "lightgray";
+    document.body.style.backgroundColor = "#fff";
+    document.getElementById("call-opacity").className = "opacity";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
+    document.getElementById("call-opacity").className = "";
 }
