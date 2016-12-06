b3f1008d1e6e9269adf34d3bba4ef6dbd154aebc
luc <luctunechi45@gmail.com>
2016-12-06 11:39:23 +0200

    new change

--- js/style.js
+++ js/style.js
@@ -17,29 +17,30 @@ $(function () {
 
 
 /*Hide and SHow Page*/
-$(document).ready(function(){
-   $("#open-side").click(function(e){
-        event.preventDefault();
-       $("#side-content-upload").hide();
-        $("#side-upload-complete").show();
-   });
-});
+// $(document).ready(function(){
+//    $("#open-side").click(function(e){
+//         event.preventDefault();
+//        $("#side-content-upload").hide();
+//         $("#side-upload-complete").show();
+//    });
+// });
+//
+// $(document).ready(function(){
+//    $("#button-save").click(function(e){
+//         event.preventDefault();
+//        $("#side-content-data").hide();
+//         $("#side-content-saved").show();
+//    });
+// });
+//
+// $(document).ready(function(){
+//    $("#button-save-file").click(function(e){
+//         event.preventDefault();
+//        $("#side-upload-complete").hide();
+//         $("#side-content-data").show();
+//    });
+// });
 
-$(document).ready(function(){
-   $("#button-save").click(function(e){
-        event.preventDefault();
-       $("#side-content-data").hide();
-        $("#side-content-saved").show();
-   });
-});
-
-$(document).ready(function(){
-   $("#button-save-file").click(function(e){
-        event.preventDefault();
-       $("#side-upload-complete").hide();
-        $("#side-content-data").show();
-   });
-});
 
 
 /*SIdebar Script*/
