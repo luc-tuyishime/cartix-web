992d1a854eedc5c7a5686c74631346190ff36664
luc <luctunechi45@gmail.com>
2016-12-06 17:14:37 +0200

    new change

--- js/style.js
+++ js/style.js
@@ -17,29 +17,29 @@ $(function () {
 
 
 /*Hide and SHow Page*/
-// $(document).ready(function(){
-//    $("#open-side").click(function(e){
-//         event.preventDefault();
-//        $("#side-content-upload").hide();
-//         $("#side-upload-complete").show();
-//    });
-// });
-//
-// $(document).ready(function(){
-//    $("#button-save").click(function(e){
-//         event.preventDefault();
-//        $("#side-content-data").hide();
-//         $("#side-content-saved").show();
-//    });
-// });
-//
-// $(document).ready(function(){
-//    $("#button-save-file").click(function(e){
-//         event.preventDefault();
-//        $("#side-upload-complete").hide();
-//         $("#side-content-data").show();
-//    });
-// });
+ $(document).ready(function(){
+    $("#button-save").click(function(e){
+         event.preventDefault();
+        $("#side-content-data").hide();
+         $("#side-content-saved").show();
+    });
+ });
+
+ // $(document).ready(function(){
+ //    $("#button-save").click(function(e){
+ //         event.preventDefault();
+ //        $("#side-content-data").hide();
+ //         $("#side-content-saved").show();
+ //    });
+ // });
+ //
+ // $(document).ready(function(){
+ //    $("#button-save-file").click(function(e){
+ //         event.preventDefault();
+ //        $("#side-upload-complete").hide();
+ //         $("#side-content-data").show();
+ //    });
+ // });
 
 
 
@@ -64,13 +64,13 @@ function closeNav() {
 }
 
 function openNav1() {
-    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("mySidenav1").style.width = "50%";
     document.body.style.backgroundColor = "#fff";
     document.getElementById("call-opacity").className = "opacity";
 }
 
 function closeNav_() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
-    document.getElementById("call-opacity").className = "";
+    document.getElementById("mySidenav1").style.width = "0";
+    document.body.style.backgroundColor = "#fff";
+    document.getElementById("call-opacity").className = "opacity";
 }
