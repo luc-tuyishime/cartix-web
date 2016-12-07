d352cb8dc9bdbc2a915d273a670faef6c11b7c76
Remy Muhire <rmuhire@exuus.com>
2016-12-07 11:51:57 +0200

    styles js

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,35 +1,31 @@
+/*Hide and SHow Page*/
+ $(document).ready(function(){
+    $("#button-save").click(function(e){
+         event.preventDefault();
+        $("#side-content-data").hide();
+         $("#side-content-saved").show();
+    });
+ });
 
-$(document).ready(function(){
-   $("#open-side").click(function(e){
-        event.preventDefault();
-       $("#side-content-upload").hide();
-        $("#side-content-data").show();
-   });
-});
+ // $(document).ready(function(){
+ //    $("#button-save").click(function(e){
+ //         event.preventDefault();
+ //        $("#side-content-data").hide();
+ //         $("#side-content-saved").show();
+ //    });
+ // });
+ //
+ // $(document).ready(function(){
+ //    $("#button-save-file").click(function(e){
+ //         event.preventDefault();
+ //        $("#side-upload-complete").hide();
+ //         $("#side-content-data").show();
+ //    });
+ // });
 
-$(document).on('click', '.btn-select', function (e) {
-    e.preventDefault();
-    var ul = $(this).find("ul");
-    if ($(this).hasClass("active")) {
-        if (ul.find("li").is(e.target)) {
-            var target = $(e.target);
-            target.addClass("selected").siblings().removeClass("selected");
-            var value = target.html();
-            $(this).find(".btn-select-input").val(value);
-            $(this).find(".btn-select-value").html(value);
-        }
-        ul.hide();
-        $(this).removeClass("active");
-    }
-    else {
-        $('.btn-select').not(this).each(function () {
-            $(this).removeClass("active").find("ul").hide();
-        });
-        ul.slideDown(300);
-        $(this).addClass("active");
-    }
-});
 
+
+/*SIdebar Script*/
 $(document).on('click', function (e) {
     var target = $(e.target).closest(".btn-select");
     if (!target.length) {
@@ -37,38 +33,3 @@ $(document).on('click', function (e) {
     }
 });
 
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "50%";
-    document.body.style.backgroundColor = "#fff";
-    document.getElementById("call-opacity").className = "opacity";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
-    document.getElementById("call-opacity").className = "";
-}
-
-function openNav1() {
-    document.getElementById("mySidenav").style.width = "50%";
-    document.body.style.backgroundColor = "#fff";
-    document.getElementById("call-opacity").className = "opacity";
-}
-
-function closeNav_() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
-    document.getElementById("call-opacity").className = "";
-}
-
-
-
-<!-- Initialize the plugin: -->
-
-/*$(document).ready(function() {
-     $('#example-selectAllNumber').multiselect({
-         includeSelectAllOption: true,
-         selectAllNumber: false
-     });
- });*/
