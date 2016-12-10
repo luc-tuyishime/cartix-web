ce36d91986f925ca442fc2b48e0106a21e9fd18a
Remy Muhire <rmuhire@exuus.com>
2016-12-10 10:25:55 +0200

    route js file

---
+++ assets/js/route.js
@@ -0,0 +1,27 @@
+myapp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/sign/sign-in.html'
+    })
+    .when('/signin', {
+        templateUrl: 'views/sign/sign-in.html'
+    })
+    .when('/signup', {
+        templateUrl: 'views/sign/sign-up.html'
+    })
+    .when('/organisation', {
+        templateUrl: 'views/sign/organisation.html'
+    })
+    .when('/new-password', {
+        templateUrl: 'views/sign/pwd-recovery.html'
+    })
+    .when('/app/',{
+        templateUrl: 'views/cartix/index.html'
+    })
+    .when('/forget-password', {
+        templateUrl:'views/sign/recovery.html' 
+    })
+    .otherwise({
+        redirectTo: '/'    
+    })
+}]);
