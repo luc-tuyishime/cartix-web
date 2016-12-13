15e244d6e1da64725e067fc2773984faeb2f4122
Remy Muhire <rmuhire@exuus.com>
2016-12-13 10:51:12 +0200

    unique_multidimensional

---
+++ assets/js/unique_multidimensional_array.js
@@ -0,0 +1,11 @@
+function multiDimensionalUnique(arr) {
+    var uniques = [];
+    var itemsFound = {};
+    for(var i = 0, l = arr.length; i < l; i++) {
+        var stringified = JSON.stringify(arr[i]);
+        if(itemsFound[stringified]) { continue; }
+        uniques.push(arr[i]);
+        itemsFound[stringified] = true;
+    }
+    return uniques;
+}
