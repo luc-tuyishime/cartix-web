3170ca1f6d3e6d93d58beefdbe30001ec99c9643
Remy Muhire <rmuhire@exuus.com>
2016-12-13 23:31:35 +0200

    route js

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,32 +1,60 @@
+var myapp = angular.module('cartixApp', ['ngRoute','ngFileUpload']);
 myapp.config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/', {
         templateUrl: 'views/sign/sign-in.html',
-        controller: 'loginBgCtrl'
+        controller: 'loginBgCtrl',
+        data: {
+            private: false
+        }
     })
     .when('/signin', {
         templateUrl: 'views/sign/sign-in.html',
-        controller: 'loginBgCtrl'
+        controller: 'signinCtrl',
+        data: {
+            private: false
+        }
     })
     .when('/signup', {
         templateUrl: 'views/sign/sign-up.html',
-        controller: 'loginBgCtrl'
+        controller: 'loginBgCtrl',
+        data: {
+            private: false
+        }
     })
     .when('/organisation', {
         templateUrl: 'views/sign/organisation.html',
-        controller: 'loginBgCtrl'
+        controller: 'loginBgCtrl',
+        data: {
+            private: false
+        }
     })
     .when('/new-password', {
         templateUrl: 'views/sign/pwd-recovery.html',
-        controller: 'loginBgCtrl'
+        controller: 'loginBgCtrl',
+        data: {
+            private: false
+        }
     })
     .when('/app/',{
         templateUrl: 'views/cartix/index.html',
-        controller: 'appBgCtrl'
+        controller: 'excelFileCtrl',
+        data: {
+            private: true
+        }
     })
     .when('/forget-password', {
         templateUrl:'views/sign/recovery.html' ,
-        controller: 'loginBgCtrl'
+        controller: 'loginBgCtrl',
+        data: {
+            private: false
+        }
+    })
+    .when('/logout', {
+      controller: 'logoutController',
+      data: {
+            private: true
+      }
     })
     .otherwise({
         redirectTo: '/'    
@@ -34,11 +62,22 @@ myapp.config(['$routeProvider', function($routeProvider){
 }]);
 
 
-myapp.controller('appBgCtrl', ['$scope', function($scope){
-    $("body").removeClass('body-login');
-    $("body").addClass('body-app');
-}]);
-myapp.controller('loginBgCtrl', ['$scope', function($scope){
-    $("body").removeClass('body-app');
-    $("body").addClass('body-login');
-}]);
+
+
+
+myapp.run(function ($rootScope, $location, $route, AuthService) {
+  $rootScope.$on('$routeChangeStart',
+    function (event, next, current) {
+        if(!AuthService.getUserStatus()){
+            if (next.data.private && !AuthService.isLoggedIn()){
+              console.log(next.data.private + " "+ AuthService.isLoggedIn());
+              $location.path('/');
+              $route.reload();
+            }
+        }
+        
+      
+  });
+});
+
+
