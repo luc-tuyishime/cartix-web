a4079d2d7290908a9964e956578e4d55085ed4e2
rmuhire <rmuhire@exuus.com>
2016-12-19 13:41:22 +0200

    controllers js

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -132,7 +132,7 @@ myapp.controller('excelFileCtrl', ['$scope', 'Upload', '$timeout', '$window', '$
     $("body").addClass('body-app');
 
     var ngo_id = restoreNgo();
-    var url = "http://api.cartix.io/api/v1/ngo/" + ngo_id;
+    var url = "http://127.0.0.1:5000/api/v1/ngo/" + ngo_id;
 
     $http.get(url).success(function(data, status, header, config) {
             $scope.ngo_name = data.ngo.name;
@@ -163,7 +163,7 @@ myapp.controller('excelFileCtrl', ['$scope', 'Upload', '$timeout', '$window', '$
 
     $scope.upload_File = function(file) {
         file.upload = Upload.upload({
-            url: 'http://api.cartix.io/api/upload/',
+            url: 'http://127.0.0.1:5000/api/upload/',
             data: {
                 file: file
             },
@@ -177,7 +177,7 @@ myapp.controller('excelFileCtrl', ['$scope', 'Upload', '$timeout', '$window', '$
                     postFailFile(response.data.originalpath, response.data.savepath, response.data.filename)
                     $scope.box_data_one = false;
                     $scope.box_data_two = true;
-                    $scope.uploadInput = false;
+                    $scope.uploadInput = true;
                     $scope.savepath = response.data.savepath;
                     $scope.filename = response.data.filename;
                 } else {
@@ -213,7 +213,7 @@ myapp.controller('excelFileCtrl', ['$scope', 'Upload', '$timeout', '$window', '$
 
         var data = '{"original":"' + original + '","save":"' + save + '","user_id":"' + id + '","filename":"' + filename + '"}';
 
-        $http.post('http://api.cartix.io/api/v1/file/save/', data, config)
+        $http.post('http://127.0.0.1:5000/api/v1/file/save/', data, config)
             .success(function(data, status, header, config) {
                 console.log(data);
             });
@@ -317,7 +317,7 @@ myapp.controller('excelFileCtrl', ['$scope', 'Upload', '$timeout', '$window', '$
 
         var data = '{"original":"' + originalpath + '","save":"","user_id":"' + user_id + '","filename":"' + filename + '"}';
 
-        $http.post('http://api.cartix.io/api/v1/file/user/', data, config)
+        $http.post('http://127.0.0.1:5000/api/v1/file/user/', data, config)
             .success(function(data, status, header, config) {
 
                 if (data.auth) {
