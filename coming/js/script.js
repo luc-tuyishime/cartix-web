8dc1f8ab9e715ab63792b28b8d9c1f307d1bef4a
rmuhire <rmuhire@exuus.com>
2016-12-21 11:06:39 +0200

    coming soon file

---
+++ coming/js/script.js
@@ -0,0 +1,32 @@
+$("document").ready(function(){
+    var i =0;
+    var x =0;
+    
+    var bgcolor = ['#001F3F','#3D9970','#6b4f63','#85144B','#111111']
+    var ran = Math.floor((Math.random() * bgcolor.length) + 1);
+    
+    $("body").css({'background-color': bgcolor[ran]});
+    
+   setInterval(function(){
+       $("#cartix_shape").css({'transform': 'rotate('+i+'deg)'});
+       i+=1;
+       if(i % 360 == 0){
+            x +=1;
+            $("body").css({'background-color': bgcolor[x]});
+            if(x == bgcolor.length){
+                x = 0;
+            }
+       }
+   },100);
+    
+    
+   
+    
+    
+    
+});
+    
+    
+    
+  
+
