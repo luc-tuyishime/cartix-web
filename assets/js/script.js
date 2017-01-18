4d83159fc8aad823df0010094264331dea76e1a6
rmuhire <rmuhire@exuus.com>
2017-01-18 20:30:21 +0200

    script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,82 +2,7 @@
 $(function() {
 
 
-    function selectBox() {
-        $('#ddlCars01').multiselect({
-            includeSelectAllOption: true
-
-        });
-        $('#ddlCars02').multiselect({
-            includeSelectAllOption: true
-        });
-        $('#ddlCars03').multiselect({
-            includeSelectAllOption: true
-        });
-        $('#ddlCars04').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars2').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars3').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars4').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars5').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars6').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars7').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars8').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars9').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars10').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars11').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars12').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars13').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars14').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars15').multiselect({
-            includeSelectAllOption: true
-        });
-
-        $('#ddlCars16').multiselect({
-            includeSelectAllOption: true
-        });
-
-    }
+    
 
 
 
@@ -137,57 +62,7 @@ $(function() {
 
 
 
-    function chartFunction() {
-        Highcharts.chart('container', {
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Browser market shares January, 2015 to May, 2015'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: [{
-                    name: 'Microsoft Internet Explorer',
-                    y: 56.33
-                }, {
-                    name: 'Chrome',
-                    y: 24.03,
-                    sliced: true,
-                    selected: true
-                }, {
-                    name: 'Firefox',
-                    y: 10.38
-                }, {
-                    name: 'Safari',
-                    y: 4.77
-                }, {
-                    name: 'Opera',
-                    y: 0.91
-                }, {
-                    name: 'Proprietary or Undetectable',
-                    y: 0.2
-                }]
-            }]
-        });
-    }
+    
     // Build the chart
 
 });
