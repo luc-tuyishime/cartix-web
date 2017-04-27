0de6bb89bbaca5c7b79da0d2ba373d68ad8441d1
rmuhire <rmuhire@exuus.com>
2017-04-27 18:00:33 +0200

    controllers js

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -994,7 +994,12 @@ function closeNav2() {
     $("#call-opacity").removeClass("opacity");
 }
 
-
+function bytesToSize(bytes) {
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   if (bytes == 0) return '0 Byte';
+   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
 
 
 function chartFunction($http, year) {
