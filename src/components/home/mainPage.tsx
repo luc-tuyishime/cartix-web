3fe08aa4862d7744158a8b28838a1c3cd2f609ea
KABIKA681 <pascalkabikampan@gmail.com>
2022-04-27 09:44:47 +0200

    Add navigation bar to the project

---
+++ src/components/home/mainPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../header/Navigation';
+
+function MainPage() {
+  return (
+      <>
+      <div className="navigation">
+        <Nav/>
+      </div>
+          <div>This is my mainPage</div>
+
+      </>
+  )
+}
+
+export default MainPage;
